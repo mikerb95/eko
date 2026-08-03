4877ef30de975959cfda029d355cd95e78f16cf8
Mike <69970540+mikerb95@users.noreply.github.com>
2026-08-03 13:00:10 -0500

    feat: add legal consent imports for compliance

--- src/pages/api/contacto.ts
+++ src/pages/api/contacto.ts
@@ -4,6 +4,7 @@ import { notifyNewContact } from '../../lib/email'
 import { queueContactAsLead } from '../../lib/zoho'
 import { defer } from '../../lib/defer'
 import { checkRateLimit, clientIp } from '../../lib/rateLimit'
+import { consentGranted, POLITICA_VERSION } from '../../lib/legal'
 
 export const prerender = false
 
